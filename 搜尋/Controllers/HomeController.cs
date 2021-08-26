95929d29831937c652fbb2d2edaa867988166893
Tom <Tom@DESKTOP-7T4KADP>
2021-08-26 10:11:25 +0800

    加入專案檔案。

---
+++ 搜尋/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 搜尋.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult 我要投資()
+        {
+            ViewBag.Message = "銀行帳號連結";
+
+            return View();
+        }
+    }
+}
---
+++ 搜尋/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="搜尋.MvcApplication" Language="C#" %>
